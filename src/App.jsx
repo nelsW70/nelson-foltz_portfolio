d6430cddafb78eac9939ff94bbee13196720c829
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-29 09:18:25 -0500

    add: router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Error from './pages/Error';
+
 import './App.css';
 
 function App() {
-  return <div>app</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Portfolio />
+        </Route>
+        <Route exact path="/resume">
+          <Resume />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error = () => {
+  return <div>Sorry. That page cannot be found.</div>;
+};
+
+export default Error;
