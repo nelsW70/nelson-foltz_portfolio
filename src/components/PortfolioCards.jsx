0d31e6cff638e623631b6fd985852aeceb035b99
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-29 15:13:54 -0500

    add: building out/destructuring card data

--- src/components/PortfolioCards.jsx
+++ src/components/PortfolioCards.jsx
@@ -4,7 +4,22 @@ import { useGlobalContext } from '../context';
 const PortfolioCards = () => {
   const { cards } = useGlobalContext();
   console.log('cards', cards);
-  return <section>i'm a buncha cards</section>;
+  return (
+    <section>
+      {cards.map((card, index) => {
+        const { image, title, role, skills, link } = card;
+        return (
+          <div key={index}>
+            <p>{image}</p>
+            <p>{title}</p>
+            <p>{role}</p>
+            <p>{skills}</p>
+            <p>{link}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default PortfolioCards;
--- src/components/PortfolioLinks.jsx
+++ src/components/PortfolioLinks.jsx
@@ -4,7 +4,13 @@ import { useGlobalContext } from '../context';
 const PortfolioLinks = () => {
   const { links } = useGlobalContext();
   console.log('links', links);
-  return <nav>i'm the portfolio links</nav>;
+  return (
+    <nav>
+      {links.map((item, index) => {
+        return <div key={index}>{item.text}</div>;
+      })}
+    </nav>
+  );
 };
 
 export default PortfolioLinks;
