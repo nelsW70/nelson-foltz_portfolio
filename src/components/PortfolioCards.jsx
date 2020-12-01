8359fbe0d7fab83ed56a98d13a96808d0b035f4f
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-01 11:06:15 -0500

    add: button a tag with links

--- src/components/PortfolioCards.jsx
+++ src/components/PortfolioCards.jsx
@@ -1,51 +1,12 @@
 import React from 'react';
 import { useGlobalContext } from '../context';
 
-// const PortfolioCards = () => {
-//   const { cards } = useGlobalContext();
-//   console.log('cards', cards);
-//   return (
-//     <section>
-//       {cards.map((card, index) => {
-//         const {
-//           image,
-//           title,
-//           description,
-//           role,
-//           skills,
-//           additionalNotes,
-//           link
-//         } = card;
-//         return (
-//           <article key={index}>
-//             <p>{image}</p>
-//             <p>{title}</p>
-//             <p>{description}</p>
-//             <p>{role}</p>
-//             <p>{skills}</p>
-//             <p>{additionalNotes}</p>
-//             <p>{link}</p>
-//           </article>
-//         );
-//       })}
-//     </section>
-//   );
-// };
 const PortfolioCards = () => {
   const { cards } = useGlobalContext();
-  console.log('cards', cards);
   return (
     <ul className="cards">
       {cards.map((card, index) => {
-        const {
-          image,
-          title,
-          description,
-          role,
-          skills,
-          additionalNotes,
-          link
-        } = card;
+        const { image, title, role, skills, link } = card;
         return (
           <li className="cards__item" key={index}>
             <div className="card">
@@ -57,7 +18,14 @@ const PortfolioCards = () => {
                 <div className="card__title">{title}</div>
                 <p className="card__text">{role}</p>
                 <p className="card__skills">{skills}</p>
-                <button className="btn btn--block card__btn">Button</button>
+                <a
+                  href={link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn--block card__btn"
+                >
+                  view site
+                </a>
               </div>
             </div>
           </li>
