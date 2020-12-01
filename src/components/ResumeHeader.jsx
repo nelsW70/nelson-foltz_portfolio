15ce4b4c2f17ee43db9c0afac4bafdef143d99cb
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-01 16:35:20 -0500

    add: 3d button to resume

--- src/components/ResumeHeader.jsx
+++ src/components/ResumeHeader.jsx
@@ -7,6 +7,20 @@ const ResumeHeader = () => {
     <header className="resume-header">
       <h1>Nelson Foltz</h1>
       <ResumeLinks />
+      <figure>
+        <div>
+          <span class="submitstory">
+            <p class="storyone">download</p>
+          </span>
+          <span class="submitstory">
+            <p class="storytwo">
+              <a href="/Nelson_Foltz.pdf" download>
+                pdf
+              </a>
+            </p>
+          </span>
+        </div>
+      </figure>
     </header>
   );
 };
