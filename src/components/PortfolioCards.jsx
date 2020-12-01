7bb8bbde10ecad30b600b4bcb892f787e147d237
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-01 08:47:38 -0500

    add: cards

--- src/components/PortfolioCards.jsx
+++ src/components/PortfolioCards.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import { useGlobalContext } from '../context';
 
+// const PortfolioCards = () => {
+//   const { cards } = useGlobalContext();
+//   console.log('cards', cards);
+//   return (
+//     <section>
+//       {cards.map((card, index) => {
+//         const {
+//           image,
+//           title,
+//           description,
+//           role,
+//           skills,
+//           additionalNotes,
+//           link
+//         } = card;
+//         return (
+//           <article key={index}>
+//             <p>{image}</p>
+//             <p>{title}</p>
+//             <p>{description}</p>
+//             <p>{role}</p>
+//             <p>{skills}</p>
+//             <p>{additionalNotes}</p>
+//             <p>{link}</p>
+//           </article>
+//         );
+//       })}
+//     </section>
+//   );
+// };
 const PortfolioCards = () => {
   const { cards } = useGlobalContext();
   console.log('cards', cards);
   return (
-    <section>
+    <ul className="cards">
       {cards.map((card, index) => {
         const {
           image,
@@ -17,18 +47,27 @@ const PortfolioCards = () => {
           link
         } = card;
         return (
-          <article key={index}>
-            <p>{image}</p>
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{role}</p>
-            <p>{skills}</p>
-            <p>{additionalNotes}</p>
-            <p>{link}</p>
-          </article>
+          <li className="cards__item" key={index}>
+            <div className="card">
+              <div
+                className="card__image card__image--fence"
+                style={{ backgroundImage: `url(${image})` }}
+              ></div>
+              <div className="card__content">
+                <div className="card__title">{title}</div>
+                <p className="card__text">
+                  This is the shorthand for flex-grow, flex-shrink and
+                  flex-basis combined. The second and third parameters
+                  (flex-shrink and flex-basis) are optional. Default is 0 1
+                  auto.{' '}
+                </p>
+                <button className="btn btn--block card__btn">Button</button>
+              </div>
+            </div>
+          </li>
         );
       })}
-    </section>
+    </ul>
   );
 };
 
