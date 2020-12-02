8a8ab9c7a40f690d09d6e75343206d738dc158cc
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-02 06:14:52 -0500

    add: error title tab

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Error = () => {
+  useEffect(() => {
+    document.title = 'Nelson Foltz | Error';
+  }, []);
   return (
     <section>
       <h1>
