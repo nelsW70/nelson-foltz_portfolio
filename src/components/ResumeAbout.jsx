6895beb625c3cb49c9f3940716cbb021554e8f41
Nelson Foltz <nelsfoltz@gmail.com>
2020-12-08 15:01:01 -0500

    add: made resume about section dynamic

--- src/components/ResumeAbout.jsx
+++ src/components/ResumeAbout.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
+import { useGlobalContext } from '../context';
 
 const ResumeAbout = () => {
+  const { resumeAbout } = useGlobalContext();
+
   return (
     <section>
       <h2>about</h2>
-      <p>
-        As a front-end developer, Nelson seeks collaborative environments where
-        thoughtful development and beauty are the goal. When he's not coding or
-        playing music, Nelson is protecting the world from his overly
-        affectionate rescue pups Molly and Dolly.
-      </p>
-      <p>
-        As a musician, Nelson Foltz has worked with such artists as Barry White,
-        Steely Dan, Aretha Franklin, Spoon, Roberta Flack, The Colorado
-        Symphony, Josh Ritter, Tony Bennett, Juliette Commagere, Dave Liebman,
-        Joachim Cooder, Rebecca Martin, Tony Williams, and Maria Schneider. His
-        credits include music for film, television and radio as well as
-        performing in the orchestra for numerous broadway shows including The
-        Lion King, Fosse, and Hairspray.
-      </p>
-      <p>
-        Foltz's original recordings are regularly featured on NPR and PRI and
-        his seventh album entitled "Departure" with longtime collaborator Tom
-        Lynn was listed as "Top Ten Tracks of 2014" by legendary music critic
-        Milo Miles (Rolling Stone/NPR/New York Times/Spin/Village Voice).
-      </p>
+      {resumeAbout.map((paragraph, index) => {
+        return <p key={index}>{paragraph}</p>;
+      })}
     </section>
   );
 };
